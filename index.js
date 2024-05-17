4e0a1e7c0205dabcd27f66f8b03ea26bd201a7f5
anish0709 <anish.singhal0709@gmai.com>
2024-05-17 12:45:41 +0530

    ready to deploy.

--- index.js
+++ index.js
@@ -32,16 +32,16 @@ app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
 
-//deployment
+// //deployment
 
-if(process.env.NODE_ENV === 'production')
-    {
-        const dirPath = path.resolve();
-        app.use(express.static('Frontend/dist'));
-        app.get("*", (req, res)=>{
-            res.sendFile(path.resolve(dirPath, 'Frontend', 'dist', 'index.html'));
-        })
-    }
+// if(process.env.NODE_ENV === 'production')
+//     {
+//         const dirPath = path.resolve();
+//         app.use(express.static('Frontend/dist'));
+//         app.get("*", (req, res)=>{
+//             res.sendFile(path.resolve(dirPath, 'Frontend', 'dist', 'index.html'));
+//         })
+//     }
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
